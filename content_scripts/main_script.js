a967ff20e5e19d3026027849300e058fb849544d
Anastasia <54910828+anastasiasoenjoto@users.noreply.github.com>
2020-02-11 16:15:55 -0500

    checks if current url is in top sites

--- content_scripts/main_script.js
+++ content_scripts/main_script.js
@@ -1,6 +1,42 @@
 function handleResponse(message) {
-    
-    console.log(message.response);
+    let urls = []
+    for (site of message.response) {
+        const u = site.url
+        if (u.includes('www')) {
+            const idx = u.indexOf('www')
+            urls.push(u.slice(idx+4))
+
+        }
+        else {
+            const idx = u.indexOf('://')
+            urls.push(u.slice(idx+3))
+       
+        }
+    }
+    console.log(urls)
+    let currentURL = window.location.href
+    if (currentURL.includes('www')) {
+        const idx = currentURL.indexOf('www')
+        currentURL = currentURL.slice(idx+4)
+
+    }
+    else {
+        const idx = currentURL.indexOf('://')
+        currentURL = currentURL.slice(idx+3)
+    }
+    console.log(currentURL)
+
+    if (urls.includes(currentURL)) {
+        // change to processing the user chosen colour
+        console.log("Site in top sites")
+    }
+    else {
+        // change to processing the user chosen colour
+        console.log("Not in top sites")
+
+
+    }
+
 }
   
 function handleError(error) {
