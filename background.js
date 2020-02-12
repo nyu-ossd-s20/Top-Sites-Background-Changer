66a6ff6272c763333cd77043cddb0bc3bb4935fd
markflaherty <47069579+markflaherty@users.noreply.github.com>
2020-02-11 20:33:07 -0500

    Add documentation

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
-
+/*
+Function gets top sites from API and returns it to the main script
+*/
 async function handleMessage(request, sender) {
+  //Get top sites 
   let sites = await browser.topSites.get();
   let tab = await browser.tabs.query({currentWindow: true, active: true})
+  //Debugging
   console.log(tab[0])
   console.log(sites);
+  //Send to main script with current tab 
   return {response: sites, current: tab[0].url}
   
 }
-
+//Call main script
 browser.runtime.onMessage.addListener(handleMessage);
 
 
